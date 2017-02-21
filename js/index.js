2d7a16e224d7a85702912b12841346694992a008
sagerioz <ericaepperson27@yahoo.com>
2017-02-21 08:14:54 -0700

    linted

--- js/index.js
+++ js/index.js
@@ -7,96 +7,68 @@ $(document).ready(function() {
   let space = ' ';
   let newNum1 = '';
   let arrNum1 = [];
-  // console.log("SCREEN", $(screen));
 
   $('span').click(function(event) {
     let operand = $(event.target).text()
-    console.log("Array Num 1", arrNum1);
 
-if ($(this).hasClass("operator") && (screen.text() === 0)){
-  return
-}else{
-
-
-
-
-    if (!$(this).hasClass("operator")) {
-      if (screen.text() == 0) {
-        screen.text("");
+    if ($(this).hasClass("operator") && (screen.text().length === 0)) {
+      return;
+    }
+    else {
+      if (!$(this).hasClass("operator")) {
+        if (screen.text() === 0) {
+          screen.text("");
+        }
+        n1 += operand;
+        screen.append(operand);
       }
-      n1 += operand;
-      console.log("n1", n1);
-      screen.append(operand);
-      // console.log(n1);
-    } else if ($(this).hasClass("operator")) {
-      console.log("I'm here....");
+      else if ($(this).hasClass("operator")) {
+        if (n1 !== "") {
+          arrNum1.push(n1);
+        }
+        if (this.innerText !== '=') {
+          operator = this.innerHTML;
+        }
+        screen.append(space + operator + space);
+        n1 = '';
 
-      if (n1 != "") {
-        arrNum1.push(n1);
-        console.log("ArrNum", arrNum1);
-      }
-      if (this.innerText !== '=') {
-        operator = this.innerHTML;
-      }
-      screen.append(space + operator + space);
-      n1 = '';
-
-      if ($(event.target).attr("id") === "clear") {
-        arrNum1 = [];
-        n1 = "";
-        screen.text("0");
-      }
-      if ($(event.target).attr("id") === "equals") {
-        switch (operator) {
-          case '+':
-            result = +(arrNum1[0]) + +(arrNum1[1])
-            screen.text(result)
-            console.log("RESULT +", result);
-            arrNum1 = [];
-            arrNum1.push(result)
-            break;
-          case '-':
-            result = +(arrNum1[0]) - +(arrNum1[1])
-            screen.text(result)
-            console.log("RESULT -", result);
-            arrNum1 = [];
-            arrNum1.push(result)
-            break;
-          case 'x':
-            result = +(arrNum1[0]) * +(arrNum1[1])
-            screen.text(result)
-            console.log("RESULT *", result);
-            arrNum1 = [];
-            arrNum1.push(result)
-            break;
-          case '÷':
-            result = +(arrNum1[0]) / +(arrNum1[1])
-            screen.text(result)
-            console.log("RESULT ÷", result);
-            arrNum1 = [];
-            arrNum1.push(result)
-            break;
-          default:
-            return "whoops";
+        if ($(event.target).attr("id") === "clear") {
+          arrNum1 = [];
+          n1 = "";
+          screen.text("");
         }
 
+        if ($(event.target).attr("id") === "equals") {
+          switch (operator) {
+            case '+':
+              result = +Number(arrNum1[0]) + +Number(arrNum1[1])
+              screen.text(result)
+              arrNum1 = [];
+              arrNum1.push(result)
+              break;
+            case '-':
+              result = +Number(arrNum1[0]) - +Number(arrNum1[1])
+              screen.text(result)
+              arrNum1 = [];
+              arrNum1.push(result)
+              break;
+            case 'x':
+              result = +Number(arrNum1[0]) * +Number(arrNum1[1])
+              screen.text(result)
+              arrNum1 = [];
+              arrNum1.push(result)
+              break;
+            case '÷':
+              result = +Number(arrNum1[0]) / +Number(arrNum1[1])
+              screen.text(result)
+              arrNum1 = [];
+              arrNum1.push(result)
+              break;
+            default:
+              return "ERROR";
+          }
+        }
       }
-
-      // console.log("MY EVENT TARGET", $(event.target).text());
-
-
     }
-
-}
-
   })
-
-
-
-
-
-
-
-
-
 })
